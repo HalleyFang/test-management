30c36b1259dce91a22c70a5ad87f9cb4782bc7b9
halley.fang <halley.fang@greaconsulting.com>
2021-07-14 19:28:06 +0800

    c init

--- src/main/java/com/testmanage/mapper/CaseTreeMapper.java
+++ src/main/java/com/testmanage/mapper/CaseTreeMapper.java
@@ -1,17 +1,20 @@
 package com.testmanage.mapper;
 
 import com.testmanage.entity.CaseTreeNode;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface CaseTreeMapper {
 
     void addTree(CaseTreeNode caseTreeNode);
 
     void updateTree(CaseTreeNode caseTreeNode);
 
-
     List<CaseTreeNode> findTree();
+
+    List<CaseTreeNode> findTreeByParent(Long parentId);
 
     CaseTreeNode findTreeById(Long id);
 }
--- src/main/java/com/testmanage/service/CaseTreeService.java
+++ src/main/java/com/testmanage/service/CaseTreeService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -22,16 +23,18 @@ public class CaseTreeService {
 
     @CacheEvict(value = "tree")
     public synchronized void addTree(JsonObject treeJson) throws Exception {
-        Map<String,Object> resultMap = analysisTreeJson(treeJson);
+        Map<String, Object> resultMap = analysisTreeJson(treeJson);
         if (!idIsExist((Long) resultMap.get("id"))) {
             updateTree(treeJson);
         } else {
             CaseTreeNode currentNode = (CaseTreeNode) resultMap.get("currentNode");
-            if(idIsExist((Long) resultMap.get("preId"))){
-                currentNode.setPre_id((Long) resultMap.get("preId"));
-            }
-            if(idIsExist((Long) resultMap.get("postId"))){
-                currentNode.setPost_id((Long) resultMap.get("postId"));
+            if (!(Boolean) resultMap.get("isOnly")) {
+                if (idIsExist((Long) resultMap.get("preId"))) {
+                    currentNode.setPre_id((Long) resultMap.get("preId"));
+                }
+                if (idIsExist((Long) resultMap.get("postId"))) {
+                    currentNode.setPost_id((Long) resultMap.get("postId"));
+                }
             }
             caseTreeMapper.addTree(currentNode);
         }
@@ -40,18 +43,15 @@ public class CaseTreeService {
 
     @CacheEvict(value = "tree")
     public synchronized void updateTree(JsonObject treeJson) throws Exception {
-        Map<String,Object> resultMap = analysisTreeJson(treeJson);
+        Map<String, Object> resultMap = analysisTreeJson(treeJson);
         CaseTreeNode currentNode = (CaseTreeNode) resultMap.get("currentNode");
         if (!idIsExist((Long) resultMap.get("id"))) {
             addTree(treeJson);
         } else {
-            CaseTreeNode treeNode = caseTreeMapper.findTreeById((Long) resultMap.get("id"));
-            if(treeNode.getPre_id() == resultMap.get("preId")) {
-                caseTreeMapper.updateTree(currentNode);//todo 没变的字段不更新
-            }else {
-                if(treeNode.getPost_id() == resultMap.get("postId")){
-                    throw new Exception("node is invalid");
-                }else {
+            if (!((boolean) resultMap.get("isOnly"))) {
+                CaseTreeNode treeNode = caseTreeMapper.findTreeById((Long) resultMap.get("id"));
+                if (treeNode.getPre_id() != resultMap.get("preId")
+                        && treeNode.getPost_id() != resultMap.get("postId")) {
                     CaseTreeNode oldPreNode = caseTreeMapper.findTreeById(treeNode.getPre_id());
                     CaseTreeNode oldPostNode = caseTreeMapper.findTreeById(treeNode.getPost_id());
                     oldPreNode.setPost_id(treeNode.getPost_id());
@@ -60,69 +60,76 @@ public class CaseTreeService {
                     caseTreeMapper.updateTree(oldPostNode);
                     currentNode.setPre_id((Long) resultMap.get("preId"));
                     currentNode.setPost_id((Long) resultMap.get("postId"));
-                    caseTreeMapper.updateTree(currentNode);
                 }
             }
+            caseTreeMapper.updateTree(currentNode);            //todo 没变的字段不更新
         }
     }
 
     private Map<String, Object> analysisTreeJson(JsonObject treeJson) throws Exception {
-        Map<String,Object> resultMap = new HashMap<>();
+        Map<String, Object> resultMap = new HashMap<>();
         JsonObject currentJson = treeJson.get("currentNode").getAsJsonObject();
-        if(currentJson == null){
+        if (currentJson == null) {
             throw new Exception("node is not invalid");
         }
         JsonObject preJson = treeJson.get("currentNode").getAsJsonObject();
         JsonObject postJson = treeJson.get("currentNode").getAsJsonObject();
         Long preId = -1L;
         Long postId = -1L;
-        if(preJson != null) {
+        Long id = currentJson.get("id").getAsLong();
+        if (preJson != null) {
             preId = preJson.get("id").getAsLong();
-            if(!idIsExist(preId)){
+            if (!idIsExist(preId)) {
                 throw new Exception("pre node is not invalid");
             }
         }
-        if(postJson != null) {
+        if (postJson != null) {
             postId = postJson.get("id").getAsLong();
-            if(!idIsExist(postId)){
+            if (!idIsExist(postId)) {
                 throw new Exception("post node is not invalid");
             }
         }
-        if(!idIsExist(preId) && !idIsExist(postId)){
+        Long parentId = currentJson.get("parentId").getAsLong();
+        List<CaseTreeNode> parentTree = caseTreeMapper.findTreeByParent(parentId);
+        Boolean isOnly = false;
+        if (parentTree.size() <= 1 && parentTree.get(0).getId().equals(id)) {
+            isOnly = true;
+        }
+        if (!idIsExist(preId) && !idIsExist(postId) && !isOnly) {
             throw new Exception("pre and post node are not invalid");
         }
 
-        Long id = currentJson.get("id").getAsLong();
-        resultMap.put("id",id);
-        resultMap.put("currentNode",jsonToNode(currentJson));
-        resultMap.put("preId",preId);
-        resultMap.put("preNode",jsonToNode(preJson));
-        resultMap.put("postId",postId);
-        resultMap.put("postNode",jsonToNode(postJson));
+        resultMap.put("id", id);
+        resultMap.put("currentNode", jsonToNode(currentJson));
+        resultMap.put("preId", preId);
+        resultMap.put("preNode", jsonToNode(preJson));
+        resultMap.put("postId", postId);
+        resultMap.put("postNode", jsonToNode(postJson));
+        resultMap.put("isOnly", isOnly);
         return resultMap;
     }
 
 
-        private Boolean idIsExist(Long id){
-            if(caseTreeMapper.findTreeById(id) instanceof CaseTreeNode){
-                return true;
-            }
-            return false;
+    private Boolean idIsExist(Long id) {
+        if (caseTreeMapper.findTreeById(id) instanceof CaseTreeNode) {
+            return true;
         }
+        return false;
+    }
 
-        private CaseTreeNode jsonToNode(JsonObject jsonObject){
-        Gson gson = new Gson();
-        return gson.fromJson(jsonObject,CaseTreeNode.class);
-        }
+    private CaseTreeNode jsonToNode(JsonObject jsonObject) {
+        Gson gson = new Gson();//todo
+        return gson.fromJson(jsonObject, CaseTreeNode.class);
+    }
 
-        private CaseTreeNode resultUpdate(Long id,CaseTreeNode currentNode){
-            CaseTreeNode treeNode = caseTreeMapper.findTreeById(id);
-            Field[] fields = treeNode.getClass().getDeclaredFields();
+    private CaseTreeNode resultUpdate(Long id, CaseTreeNode currentNode) {
+        CaseTreeNode treeNode = caseTreeMapper.findTreeById(id);
+        Field[] fields = treeNode.getClass().getDeclaredFields();
            /* for(Field field : fields){
 
             }*/
-            return treeNode;//todo
-        }
+        return treeNode;//todo
+    }
 
     @Cacheable(value = "tree")
     public String getTree() {
